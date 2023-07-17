46e2484f15d009010a9a61476cedb29627813666
Ahmed39-wq <ahmedjr39@gmail.com>
2023-07-17 16:40:16 +0000

    intro to view engine

--- app.js
+++ app.js
@@ -2,28 +2,32 @@ const express = require('express')
 
 const app = express()
 
+//Register view engine
+app.set('view engine', 'ejs')
+
 //listen for requests
 app.listen(3000)
 
 app.get('/', (req, res) => {
     // res.send('<p>Home Page</p>')
-    res.sendFile('./servers/views/index.html', { root: __dirname})
+    res.render('index', { title: 'Home' })
 })
 
 app.get('/about', (req, res) => {
     // res.send('<p>Home Page</p>')
-    res.sendFile('./servers/views/about.html', { root: __dirname})
-
+    res.render('about', { title: 'About' })
 })
 
-
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'Create Blog' })
+})
 //redirects
-app.get('/about-us', (req, res) => {
-    res.redirect('/about')
-})
+// app.get('/about-us', (req, res) => {
+//     res.redirect('/about')
+// })
 
 
 //404 page
 app.use((req, res) => {
-    res.status(400).sendFile('./servers/views/404.html', { root: __dirname})
+    res.status(404).render('404', { title: '404' })
 })
