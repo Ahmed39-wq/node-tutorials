6c73709d205fab7d72042b2e185e44909239fe9e
Ahmed39-wq <ahmedjr39@gmail.com>
2023-07-18 08:45:38 +0000

    view engine-ejs rendering

--- app.js
+++ app.js
@@ -10,7 +10,13 @@ app.listen(3000)
 
 app.get('/', (req, res) => {
     // res.send('<p>Home Page</p>')
-    res.render('index', { title: 'Home' })
+    const blogs = [
+        { title: 'Marina Bay', snippet: 'lorem ipsum dolor sit amet, consectetur adip'},
+        { title: 'Seychelles', snippet: 'lorem ipsum dolor sit amet, consectetur adip'},
+        { title: 'Ibiza', snippet: 'lorem ipsum dolor sit amet, consectetur adip'},
+        { title: 'Miami', snippet: 'lorem ipsum dolor sit amet, consectetur adip'}
+    ]
+    res.render('index', { title: 'Home', blogs})
 })
 
 app.get('/about', (req, res) => {
