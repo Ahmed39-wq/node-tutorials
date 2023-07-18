a7cebaa71a5febf3a1553f12528c835b72b7aba0
Ahmed39-wq <ahmedjr39@gmail.com>
2023-07-18 11:38:18 +0000

    introduction to middleware

--- app.js
+++ app.js
@@ -8,6 +8,21 @@ app.set('view engine', 'ejs')
 //listen for requests
 app.listen(3000)
 
+//this leaves the current request hanging when the middleware runs without the next()
+app.use((req, res, next) => {
+    console.log('new request made:');
+    console.log('host:', req.hostname);
+    console.log('path:', req.path);
+    console.log('method:', req.method);
+    next(); // this tells the middleware to move on to the next request
+}) 
+
+app.use((req, res, next) => {
+    console.log('in the next middleware');
+    next(); 
+})
+
+//Route handlers
 app.get('/', (req, res) => {
     // res.send('<p>Home Page</p>')
     const blogs = [
@@ -27,6 +42,7 @@ app.get('/about', (req, res) => {
 app.get('/blogs/create', (req, res) => {
     res.render('create', { title: 'Create Blog' })
 })
+
 //redirects
 // app.get('/about-us', (req, res) => {
 //     res.redirect('/about')
@@ -37,3 +53,5 @@ app.get('/blogs/create', (req, res) => {
 app.use((req, res) => {
     res.status(404).render('404', { title: '404' })
 })
+
+//Middleware runs between requests and responses, thus, functions that are executed in the middle of processing an http request-response cycle
